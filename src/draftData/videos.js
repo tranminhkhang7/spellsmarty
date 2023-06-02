d246a3daf252e80b17d11d6c31c066f5b1056a4a
noday-off <thinn2002@gmail.com>
2023-06-02 16:04:53 +0700

    search page

---
+++ src/draftData/videos.js
@@ -0,0 +1,87 @@
+const Videos = [
+	{
+		id: 1,
+		thumbnail: 'https://i.ytimg.com/vi/IB6rV7sNphA/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAAwM6rkVLJO8buQm32YY9W_EVNfA',
+		length: '1:06:43',
+		title: 'Ramana Maharshi on self-enquiry and the Guru',
+		views: '47K',
+		category: 'Education',
+		channel: {
+			name: 'David Godman',
+			avatar: 'https://yt3.googleusercontent.com/ytc/AGIKgqM4r8-tRSxhIhx3ZGecu693eYhh32I4xkyjbhfcnw=s176-c-k-c0x00ffffff-no-rj',
+		}
+	},
+	{
+		id: 2,
+		thumbnail: 'https://i.ytimg.com/vi/hVYv9ktilQw/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAxmxLAMHkns37iWyWN2vOirIGreQ',
+		length: '1:20:00',
+		title: 'Sri Ramana Maharshi - JNANI',
+		views: '3M',
+		category: 'Education',
+		channel: {
+			name: 'CINEFX PRODUCTIONS',
+			avatar: 'https://yt3.googleusercontent.com/asFTuesFjdISGvB7zLFm7RVWFMhRnH9RLjeWwgU-8oVMycHLI4ktJQwhZdpid5Em-h0fwOoU=s176-c-k-c0x00ffffff-no-rj',
+		}
+	},
+	{
+		id: 3,
+		thumbnail: 'https://i.ytimg.com/vi/X7vVP7F3-wM/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLC1_-etC3z-ke7C2hqkMkaxidaC5g',
+		length: '10:15',
+		title: 'Interview with Google CEO Sundar Pichai at Google ...',
+		views: '2.4M',
+		category: 'Podcast',
+		channel: {
+			name: 'iJustine',
+			avatar: 'https://yt3.googleusercontent.com/TJTGhrVOoYxtaMg5GL5GoNYkzFrmIieMA4-AeRecbstcc-rWnTcrw8Vt1Ea1t6QBx9o_CIixFP8=s176-c-k-c0x00ffffff-no-rj',
+		}
+	},
+	{
+		id: 4,
+		thumbnail: 'https://i.ytimg.com/vi/IavOCbzFDUk/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAaZzv5NX4SSuGKUQx4jcVzM2Crmg',
+		length: '6:02',
+		title: 'Gravitas: Gen Z says no to kids',
+		views: '22K',
+		category: 'Education',
+		channel: {
+			name: 'WION',
+			avatar: 'https://yt3.googleusercontent.com/ytc/AGIKgqNWtVPoF3ORtRIfLDpK2NUfNQf8sM8Ho7zpOXQ-sw=s176-c-k-c0x00ffffff-no-rj',
+		}
+	},
+	{
+		id: 5,
+		thumbnail: 'https://i.ytimg.com/vi/75d_29QWELk/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLA_z-P3zQyCtp91UvN8HBnAcD21dA',
+		length: '11:31',
+		title: 'Change Your Life - One TIny Step at a Time',
+		views: '8.3M',
+		category: 'Education',
+		channel: {
+			name: 'Kurzgetsagt - In a Nutshell',
+			avatar: 'https://yt3.googleusercontent.com/ytc/AGIKgqOibtncbyNaJVeUjVotNRl0r00hkiUfYEEv5XmNdw=s176-c-k-c0x00ffffff-no-rj',
+		}
+	},
+	{
+		id: 6,
+		thumbnail: 'https://i.ytimg.com/vi/jPhJbKBuNnA/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLB-_v3PfgbWUr3iZ5l5bFhTgy_quQ',
+		length: '15:49',
+		title: 'I tried using AI. It scared me.',
+		views: '4.9M',
+		category: 'Vlog',
+		channel: {
+			name: 'Tom Scott',
+			avatar: 'https://yt3.googleusercontent.com/ytc/AGIKgqPKsRiD8L9lbIJJGo5S6uYGG_S2zgyB1XkAmKpMJA=s176-c-k-c0x00ffffff-no-rj',
+		}
+	},
+	{
+		id: 7,
+		thumbnail: 'https://i.ytimg.com/vi/dtfaccGmCCs/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLASgS75FGyG6oHMuaVS8hsxVQoPJA',
+		length: '14:10',
+		title: "You Don't Actually Know What Your Future Self Wants | Shankar Vedantam | TED",
+		views: '930K',
+		category: 'Education',
+		channel: {
+			name: 'TED',
+			avatar: 'https://yt3.googleusercontent.com/ytc/AGIKgqNnFV3H1lIKv43VSVdknMH6aJPNAbyTmpSK173s3w=s176-c-k-c0x00ffffff-no-rj',
+		}
+	},
+];
+export default Videos;
---
+++ src/pages/search.jsx
@@ -0,0 +1,94 @@
+import React, { useEffect } from 'react';
+import './Search.css';
+import Videos from '../draftData/videos';
+import { useState } from 'react';
+import VideoCard from './VideoCard';
+
+const SearchPage = () =>{
+	const [data, setData] = useState(Videos);
+	const [filteredData,setFilteredData] = useState(data);
+	const [keyword, setKeyword] = useState('');
+	
+	useEffect(()=>{
+		console.log(keyword);
+		setFilteredData(data.filter(videos => videos.title.toLowerCase().includes(keyword.toLowerCase())));
+	},[keyword]);
+
+	const searchDynamic = (e) => {
+		setKeyword(e.target.value);
+	}
+
+	const handleSearch = (e) => {
+		e.preventDefault();
+		console.log(e.target.category);
+		setFilteredData(data.filter(video => video.title.toLowerCase().includes(keyword.toLocaleLowerCase())));
+	}
+
+	return (
+		<div className='search-form'>
+		{/* <div id="background-item"></div> */}
+		<div className="flex items-center mb-4">
+			<form action="/search" method="GET" className="w-full" onSubmit={handleSearch}>
+			<div className="flex items-center content-center rounded-r-md mb-10">
+				<input type="text"
+					value={keyword}
+					onChange={searchDynamic}
+					placeholder="How America’s richest donate their money" 
+					class="px-4 py-2 w-full rounded-l-md border hover:border-grey-300/50 focus:outline-none focus:ring-2 focus:ring-blue-500/50 text-center" />
+				<button onClick={handleSearch}
+					class="px-4 py-2 text-black rounded-r-md hover:bg-blue-500/50 focus:outline-none focus:ring-2 focus:ring-blue-500">
+					Search
+				</button>
+			</div>
+				<div className="flex items-center justify-between mb-4 ">
+					<select className='w-1/5 px-4 py-2 rounded-md border border-gray-300 text-center text-grey-500 focus:outline-none focus:ring-1 focus:ring-blue-500/50 '>
+						<option value="" disabled selected hidden>Level</option>
+						<option value="option1">A1</option>
+						<option value="option1">A2</option>
+						<option value="option1">B1</option>
+						<option value="option2">B2</option>
+						<option value="option3">C1</option>
+					</select>
+					
+					<select className='w-1/5 px-4 py-2 rounded-md border border-gray-300 text-center text-grey-500 focus:outline-none focus:ring-1 focus:ring-blue-500/50'>
+						<option value="" disabled selected hidden>Length</option>
+						<option value="option1"> ~ 1 minutes</option>
+						<option value="option2"> ~ 5 minutes</option>
+						<option value="option3"> ~ 10 minutes</option>
+						<option value="option3"> over 10 minutes</option>
+					</select>
+					
+					<select name='category' className='w-1/5 px-4 py-2 rounded-md border border-gray-300 text-center text-grey-500 focus:outline-none focus:ring-1 focus:ring-blue-500/50'>
+						<option value="" disabled selected hidden>Category</option>
+						<option value="option1">Education</option>
+						<option value="option2">Podcast</option>
+						<option value="option3">Vlog</option>
+					</select>
+					
+					<select className='w-1/5 px-4 py-2 rounded-md border border-gray-300 text-center text-grey-500 focus:outline-none focus:ring-1 focus:ring-blue-500/50'>
+						<option value="" disabled selected hidden>Accent</option>
+						<option value="option1">Option 1</option>
+						<option value="option2">Option 2</option>
+						<option value="option3">Option 3</option>
+					</select>
+				</div>
+			</form>
+			</div>
+		<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+			{filteredData.map((video) => (
+				<VideoCard
+				key={video.id}
+				thumbnail={video.thumbnail}
+				length={video.length}
+				title={video.title}
+				channelAvatar={video.channel.avatar}
+				channelName={video.channel.name}
+				views={video.views}
+				/>
+			))}
+    	</div>
+		</div>
+	);
+}
+
+export default SearchPage;
