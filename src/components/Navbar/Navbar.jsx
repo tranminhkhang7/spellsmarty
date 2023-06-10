e802a3d2992bded5f403226fc87516bd691d7ecb
khoabm <buiminhkhoa2706@gmail.com>
2023-06-10 20:14:23 +0700

    change user status after login

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,14 @@
-import { FiLogIn, FiUserPlus } from 'react-icons/fi';
+import { FiLogIn, FiUserPlus, FiUser } from 'react-icons/fi';
+import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 const Navbar = () => {
+  const [token, setToken] = useState('');
+  const [userName, setUserName] = useState('');
+  useEffect(() => {
+    // Check if token is present in localStorage
+    setToken(localStorage.getItem('token'));
+    setUserName(localStorage.getItem('username'));
+  }, []);
   return (
     <nav className="bg-gray-950 py-4 px-8 flex flex-wrap items-center">
       <div className="text-2xl font-bold text-white">
@@ -36,14 +44,25 @@ const Navbar = () => {
         </li> */}
       </ul>
       <div className="ml-auto flex space-x-6">
-        <Link to="/signin" className="flex items-center text-white hover:text-gray-600">
-          <FiLogIn className="mr-2" />
-          Login
-        </Link>
-        <Link to="/signup" className="flex items-center text-white hover:text-gray-600">
-          <FiUserPlus className="mr-2" />
-          Sign Up
-        </Link>
+        {token ? (
+          <Link to="/profile">
+            <div className="flex items-center text-white overflow-hidden whitespace-nowrap">
+              <FiUser className="mr-2" />
+              {userName}
+            </div>
+          </Link>
+        ) : (
+          <>
+            <Link to="/signin" className="flex items-center text-white hover:text-gray-600">
+              <FiLogIn className="mr-2" />
+              Login
+            </Link>
+            <Link to="/signup" className="flex items-center text-white hover:text-gray-600">
+              <FiUserPlus className="mr-2" />
+              Sign Up
+            </Link>
+          </>
+        )}
       </div>
     </nav>
   );
