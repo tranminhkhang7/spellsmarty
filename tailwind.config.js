7c47031acc43e6df3d636c815c029fe918f04654
khoabm <buiminhkhoa2706@gmail.com>
2023-06-12 00:34:30 +0700

    exclude tailwind forms

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,11 +9,19 @@ module.exports = {
         secondary: '#F2F4E6',
         browne: '#b7783a',
       },
+      minWidth: {
+        36: '9rem',
+        44: '11rem',
+        56: '14rem',
+        60: '15rem',
+        72: '18rem',
+        80: '20rem',
+      },
     },
   },
   plugins: [
     // eslint-disable-next-line global-require
-    require('@tailwindcss/forms'),
+    // require('@tailwindcss/forms'),
     // add custom variant for expanding sidebar
     plugin(({ addVariant, e }) => {
       addVariant('sidebar-expanded', ({ modifySelectors, separator }) => {
